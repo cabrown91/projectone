cca576649f77f9ebaef6ea218c6fcfb06378c9eb
cabrown91 <cleo.aliciabrown@gmail.com>
2016-07-22 16:05:18 -0700

    require donation.js

--- models/index.js
+++ models/index.js
@@ -4,3 +4,5 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/projectone");
 module.exports.Nonprofit = require("./nonprofit.js");
 
 module.exports.User = require("./user.js");
+
+module.exports.Donation = require("./donation.js");
