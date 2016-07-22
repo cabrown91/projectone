557665d2200ddcfdc4368dc8625b706e151cb02f
cabrown91 <cleo.aliciabrown@gmail.com>
2016-07-22 16:15:44 -0700

    require join table nonprofit_donation

--- models/index.js
+++ models/index.js
@@ -6,3 +6,5 @@ module.exports.Nonprofit = require("./nonprofit.js");
 module.exports.User = require("./user.js");
 
 module.exports.Donation = require("./donation.js");
+
+module.exports.NonprofitDonation = require("./nonprofit_donation.js");
